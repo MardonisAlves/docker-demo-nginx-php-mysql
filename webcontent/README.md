c0491aec90f92aee973bf922b47cbd226c4297bb
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-11-06 19:46:03 -0300

    demo-docker

---
+++ webcontent/README.md
@@ -0,0 +1,1 @@
+<h1>Pricipais comandos Docker </h1>
---
+++ webcontent/index.php
@@ -0,0 +1,36 @@
+
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php  
+ $servername = "docker-mysql-demo"; // AQUI é nome do container docker mysql
+ $username = "root";
+ $password = "root123";
+ $dbname = "docker_demo";
+ $port = "3306";
+ 
+ try{
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname",$username,$password);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected succesfully";
+ } catch(PDOException $e){
+    echo "Connection failed: " . $e -> getMessage();
+ }
+
+
+ $sql = 'SELECT * FROM  users';
+ foreach ($conn->query($sql) as $row) {
+     print $row['name'] . "<br />";
+     print $row['email'] . "<br />";
+ }
+
+ $lines = file('README.md');
+ 
+ foreach ($lines as $line) {
+   echo "$line";
+}
+ ?> 
+ </body>
+</html>
